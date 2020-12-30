03dfa096b2171f8c68888d0d444cf62b5f248a4e
ksg <ksg050304@naver.com>
2020-12-31 08:52:45 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+const welcomeChannelName = "🙆ㅣ안녕하세요";
+const byeChannelName = "🙇ㅣ안녕히가세요";
+const welcomeChannelComment = "님 KENZO SEVERR에 오신것을 환영합니다 .";
+const byeChannelComment = "님이 KENZO SERVER에서 나가셨습니다";
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
+client.on('message', (message) => {
+  if(message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
